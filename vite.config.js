557e81fdca1af833e15b1219b174020792373e8f
SelwynatorMC <marc.cole@wales.nhs.uk>
2025-08-27 16:47:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins:[react()], base:'/CHANGE_ME_REPO_NAME/' })
+export default defineConfig({ plugins:[react()], base:'/extranet-diagram/' })
